3b583fee148935b2cc5616f745eb4fcdd2ce532f
Gabinator80 <cacaaunoisette80@gmail.com>
2023-10-07 16:15:26 +0200

    ajout de l'index html

---
+++ script.js
@@ -0,0 +1,15 @@
+// const slidbox = document.querySelector('.p-1');
+// // alert("test")
+// window.addEventListener('scroll', () => {
+// 	const {scrollTop, clientHeight} = document.documentElement;
+
+// 	const topelement = slidbox.getBoundingClientRect().top;
+
+
+// 	if(scrollTop > (scrollTop + topelement).toFixed()) {
+// 		Slidebox.classList.add('active');
+// 	}
+
+
+
+// });

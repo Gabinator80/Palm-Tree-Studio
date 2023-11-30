205bde47809bd4ee6506afeb3bffcde5a781a611
Gabinator80 <107307753+Gabinator80@users.noreply.github.com>
2023-11-30 18:24:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// const slidbox = document.querySelector('.p-1');
-// // alert("test")
-// window.addEventListener('scroll', () => {
-// 	const {scrollTop, clientHeight} = document.documentElement;
-
-// 	const topelement = slidbox.getBoundingClientRect().top;
+const counterEl = document.querrySelector('.counter p ');
 
 
-// 	if(scrollTop > (scrollTop + topelement).toFixed()) {
-// 		Slidebox.classList.add('active');
-// 	}
+let count = 10000;
 
 
+setInterval(() => {
+  count = count-1;
+   if(counter <= 0) {
+  count = 0;
+  }
+  counterEl.textContent = count;
 
-// });
+
+}, 1000);
+
+            
